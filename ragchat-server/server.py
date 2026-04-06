89dc72e61e75540ae1abbfe8f826227612c27d91
zczhao1992 <zczhao1992@163.com>
2026-04-06 20:29:13 +0800

    feat: 为部署修改配置

--- ragchat-server/server.py
+++ ragchat-server/server.py
@@ -4,37 +4,39 @@ import sys
 import signal
 import asyncio
 import platform
+import os
 
 
 def handle_shutdown(signum, frame):
-    print("shut down...")
+    print("Shutting down...")
     sys.exit(0)
 
 
 signal.signal(signal.SIGINT, handle_shutdown)
 signal.signal(signal.SIGTERM, handle_shutdown)
 
-
 if __name__ == "__main__":
-
     if platform.system() == "Windows":
-        # 1. 强制设置策略为 Selector
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    env_port = int(os.environ.get("PORT", 8000))
+
+    env_host = "0.0.0.0" if os.environ.get("RENDER") else "localhost"
 
     parser = argparse.ArgumentParser(description="Run the RAGCHAT server")
 
     parser.add_argument(
         "--host",
         type=str,
-        default="localhost",
-        help="host bind the server to(default: localhost)"
+        default=env_host,
+        help=f"host bind the server to (default: {env_host})"
     )
 
     parser.add_argument(
         "--port",
         type=int,
-        default=8000,
-        help="host bind the server to(default: 8000)"
+        default=env_port,
+        help=f"port bind the server to (default: {env_port})"
     )
 
     parser.add_argument(
@@ -47,19 +49,22 @@ if __name__ == "__main__":
         "--log-level",
         type=str,
         default="info",
-        choices=["critical", "error", "warning", "debug"],
+        choices=["critical", "error", "warning",
+                 "debug", "info"],
         help="log level (default: info)"
     )
 
     args = parser.parse_args()
 
-    reload = False
-    if args.reload:
-        reload = True
-
     try:
-        uvicorn.run("src.main:app", host=args.host,
-                    port=args.port, reload=reload, log_level=args.log_level, loop="asyncio")
+        print(f"Starting server on {args.host}:{args.port}")
+        uvicorn.run(
+            "src.main:app",
+            host=args.host,
+            port=args.port,
+            reload=args.reload,
+            log_level=args.log_level
+        )
     except Exception as e:
-        print(e)
+        print(f"Error occurred: {e}")
         sys.exit(1)
